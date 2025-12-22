53a27621e3e9b1d7ebb004131980b1dd96ded848
sjin4861 <sjin4861@postech.ac.kr>
2025-12-22 06:10:18 +0000

    Add ./src/pykt-toolkit/pykt/config/config.py

---
+++ src/pykt-toolkit/pykt/config/config.py
@@ -0,0 +1,78 @@
+que_type_models = [
+    "iekt",
+    "qdkt",
+    "qikt",
+    "lpkt",
+    "rkt",
+    "akt_que",
+    "akt_que",
+    "iekt_que",
+    "qikt_que",
+    "simplekt_que",
+    "sparsekt_que",
+    "kqn_que",
+    "atkt_que",
+    "deep_irt_que",
+    "dkvmn_que",
+    "skvmn_que",
+    "dkt_plus_que",
+    "atdkt_que",
+    "sakt_que",
+    "saint_que",
+    "polya_fusion_qdkt",
+    "cognitive_deep_irt",
+    "neuro_cognitive_akt",
+    "neuro_cognitive_simplekt",
+    "neuro_cognitive_qdkt",
+    "neuro_cognitive_transformer",
+    "cp_qikt",
+    "polya_akt_v5",
+    "polya_qdkt_v5",
+    "polya_simplekt_v5",
+    "polya_qikt_v5",
+    "polya_sparsekt_v5",
+]
+
+qikt_ab_models = [
+    "qikt_ab_a+b+c",
+    "qikt_ab_a+b+c+irt",
+    "qikt_ab_a+b+irt",
+    "qikt_ab_a+c+irt",
+    "qikt_ab_a+irt",
+    "qikt_ab_b+irt",
+]
+
+que_type_models += qikt_ab_models
+
+## We add a dictionary to find the mapping between original model and their implemented que_type versions.
+dict_baseline_to_model = {
+    "akt": "akt_que",
+    "atdkt": "atdkt_que",
+    "atkt": "atkt_que",
+    "deep_irt": "deep_irt_que",
+    "dkt": "qdkt",
+    "dkt+": "dkt_plus_que",
+    "dkvmn": "dkvmn_que",
+    "iekt": "iekt_que",
+    "kqn": "kqn_que",
+    "qdkt": "qdkt",
+    "qikt": "qikt_que",
+    "saint": "saint_que",
+    "sakt": "sakt_que",
+    "simplekt": "simplekt_que",
+    "sparsekt": "sparsekt_que",
+    "cognitive_deep_irt": "cognitive_deep_irt",
+    "neuro_cognitive_akt": "neuro_cognitive_akt",
+    "neuro_cognitive_simplekt": "neuro_cognitive_simplekt",
+    "neuro_cognitive_qdkt": "neuro_cognitive_qdkt",
+    "neuro_cognitive_transformer": "neuro_cognitive_transformer",
+    "cp_qikt": "cp_qikt",
+    "polya_akt_v5": "polya_akt_v5",
+    "polya_qdkt_v5": "polya_qdkt_v5",
+    "polya_simplekt_v5": "polya_simplekt_v5",
+    "polya_qikt_v5": "polya_qikt_v5",
+    "polya_sparsekt_v5": "polya_sparsekt_v5",
+}
+
+# Reversing the above dictionary
+dict_model_to_baseline = {v: k for k, v in dict_baseline_to_model.items()}
